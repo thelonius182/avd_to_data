79aaffb4969e476c2670361f255746d878892aa0
Lon <lcavdakker@gmail.com>
2022-07-28 00:58:29 +0200

    vanilla

---
+++ src/convert2data_drive.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(magrittr)
+library(readr)
+library(fs)
+
+rlprg_files <- dir_ls(path = "/home/lon/R_projects/avd_to_data/resources/AVD2DATA_files",
+                      type = "file",
+                      regexp = "[.]rlprg") %>% as_tibble()
+
+for (a_file in rlprg_files$value) {
+  
+  oe_raw <- read_file(file = a_file) %>% as_tibble()
+  
+  oe_data <- oe_raw %>% str_replace_all(pattern = "/Volumes/Avonden/", replacement = "/Volumes/Data/")
+  
+  oe_data_file_name <- str_replace(a_file, pattern = "/AVD2DATA_files/", replacement = "/AVD2DATA_conv_files/")
+  
+  write_file(oe_data, file = oe_data_file_name)
+}
