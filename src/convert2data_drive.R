6e217e2f8ac0f4955cdfc396e5ffcc64c40ef632
Lon <lcavdakker@gmail.com>
2022-07-28 23:29:04 +0200

    productionrun

--- src/convert2data_drive.R
+++ src/convert2data_drive.R
@@ -3,7 +3,7 @@ library(magrittr)
 library(readr)
 library(fs)
 
-rlprg_files <- dir_ls(path = "/home/lon/R_projects/avd_to_data/resources/AVD2DATA_files",
+rlprg_files <- dir_ls(path = "/home/lon/R_projects/avd_to_data/resources/AVD2DATA_files/Programs/",
                       type = "file",
                       regexp = "[.]rlprg") %>% as_tibble()
 
@@ -13,7 +13,7 @@ for (a_file in rlprg_files$value) {
   
   oe_data <- oe_raw %>% str_replace_all(pattern = "/Volumes/Avonden/", replacement = "/Volumes/Data/")
   
-  oe_data_file_name <- str_replace(a_file, pattern = "/AVD2DATA_files/", replacement = "/AVD2DATA_conv_files/")
+  oe_data_file_name <- str_replace(a_file, pattern = "/AVD2DATA_files/Programs/", replacement = "/AVD2DATA_conv_files/")
   
   write_file(oe_data, file = oe_data_file_name)
 }
